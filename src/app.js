fbc9eab1cf2755850d573abed88625eec2558901
harinaik <harinaikmeravath@gmail.com>
2024-03-03 20:52:17 +0530

    change message

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/testNode", (req, res) => {
-  res.status(200).send("yes the testNode endpoint works");
+  res
+    .status(200)
+    .send("yes the testNode endpoint worked and returned correct value");
 });
 
 module.exports = app;
